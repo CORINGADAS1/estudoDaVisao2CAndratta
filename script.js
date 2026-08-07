a89834a6c93aeec7c1a7cf24cf43debad5417e39
CORINGADAS1 <kauane.conerado@escola.pr.gov.br>
2026-08-07 09:03:57 -0300

    ok

---
+++ script.js
@@ -0,0 +1,26 @@
+//botões de acessibilidade
+document.addEventListener("DOMContentLoaded, () =>{
+    
+const btnContraste = document.getElementById("btn-contraste");
+const btnAumentar = document.getElementById("btn-aumentar-texto");
+const btnDiminuir = document.getElementById("btn-diminuir-texto");
+
+// aumenta a fonte
+
+let tamanhoAtualfonte = 100;
+
+//função alto contraste
+
+btnContraste.addEventListener("click", () =>{
+    
+document.body.classList.toogle("alto-contraste");
+
+//acessibilidade para leitores de tela 
+
+const ativo = document.body.classList.contains("alto contraste");
+btnContraste.setAttrbute("aria-pressed", ativo);
+
+)
+
+
+}
