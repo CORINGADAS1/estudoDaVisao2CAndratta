dc5be78e335548efc4ea26d9bc97849d0a29f7ae
CORINGADAS1 <kauane.conerado@escola.pr.gov.br>
2026-08-14 08:51:38 -0300

    ok

--- script.js
+++ script.js
@@ -20,7 +20,24 @@ document.body.classList.toogle("alto-contraste");
 const ativo = document.body.classList.contains("alto contraste");
 btnContraste.setAttrbute("aria-pressed", ativo);
 
-)
+)}
 
+//função aumentar o texto
 
-}
+btnAumentar.addEventListener("click", ()=>{
+    if (tamanhoAtualfonte < 150){
+        tamanhoAtualfonte += 10;
+        document.documentElement.style.fontSize = `${tamanhoAtualfonte}%`
+    }
+});
+
+//função diminuir texto;
+
+btnDiminuir.addEventListener("click", ()=>{
+    if(tamanhoAtualfonte > 90){
+        tamanhoAtualfonte .= 10;
+    }
+        document.documentElement.style.fontSize = `${tamanhoAtualfonte}%`
+    }
+
+})
